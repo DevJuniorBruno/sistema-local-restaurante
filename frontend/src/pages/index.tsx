2c7d7f1026a43e29ce9b4485598a2e00f97573f6
DevJuniorBruno <brunode2001@gmail.com>
2023-02-10 08:04:08 -0300

    add sass

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,31 @@
+import Head from 'next/head';
+import styles from '../../styles/home.module.scss';
+import Image from 'next/image';
+import {Input} from '../components/ui/input'
+import { TextArea } from '../components/ui/input';
 
-
+import logoImg from '../../public/logo.svg';
 
 export default function Home() {
   return (
     <div>
-      <h1>DevJuniorBruno</h1>     
+      <Head>
+      <title>SujeitoPizza - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image  src={logoImg} alt='logoImage'  />
+      </div>
+      <div>
+        <form>
+          <Input
+          placeholder='Digite seu login'
+          />
+
+          <TextArea  
+            placeholder='Digite sua senha'
+          />
+        </form>
+      </div>
     </div>
   )
 }
